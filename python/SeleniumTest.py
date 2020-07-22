f28f7b83d1fb715280ef6a9998475cc16ed88194
vhvieira <victor@alphatecti.com.br>
2020-07-22 10:10:59 -0300

    First version of whatsapp sending message

--- python/SeleniumTest.py
+++ python/SeleniumTest.py
@@ -10,26 +10,38 @@ class WhatsappBot:
         self.to = ["Paulo Nunes"]
         self.options = webdriver.ChromeOptions()
         self.options.add_argument("lang=pt-br")
-        self.options.add_argument("user-data-dir=c:\\Users\\vhrodriguesv\\AppData\\Local\\Google\\Chrome\\User");
+        self.options.add_argument("user-data-dir=temp");
         self.driver =  webdriver.Chrome(executable_path=r'./chromedriver.exe', options=self.options)
 
     #Method to send message
+    #TODO: Read from a config file
+    #TODO: Read new messages method
+    #TODO: V1: How to read a file in Phyton (Java Endpoint will override the message files in the dir, phyton will read and send)
+    #TODO: V2: Endpoint in Python then it directs modify the objects and not need for IO operations
     def SendMessages(self):
         print('Iniciando whatsapp robot')
         self.driver.get("https://web.whatsapp.com")
-        time.sleep(30)
+        time.sleep(10)
         #If we want to keep reading message, we can create a infinity loop here
+        #Conversations are class div class="_210SC"
         for dest in self.to:
-            self.driver.find_element_by_xpath(f"//span[@title'{dest}']")
+            print('Enviando mensagem para:' + dest)
+            conversation = self.driver.find_element_by_xpath(f"//span[@title='{dest}']")
             time.sleep(3)
-            chatbox = self.driver.find_element_by_class_name("_13mgZ")
+            print('Conversa encontrada')
+            conversation.click()
+            #[0] is search in the conversation, [1] is chatbox
+            chatboxes = self.driver.find_elements_by_class_name("_3FRCZ")
             time.sleep(3)
-            chatbox.click()
-            chatbox.send_keys(self.message)
-            send_button = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+            print('Chatboxs encontrados: ' + str(len(chatboxes)))
+            chatboxes[1].click()
+            chatboxes[1].send_keys(self.message)
+            send_icon= self.driver.find_element_by_xpath("//span[@data-icon='send']")
             time.sleep(3)
-            send_button.click()
+            print('Send icon encontrado')
+            send_icon.click()
             time.sleep(5)
+            print('Mensagem enviada para:' + dest)
 
 bot = WhatsappBot()
 bot.SendMessages()
