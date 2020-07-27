1c9b3ded985cefa6727583edbb31f95421d1ce39
vhvieira <victor@alphatecti.com.br>
2020-07-27 14:09:42 -0300

    First papagaio version

--- python/whatsbot/classes/WhatsappBot.py
+++ python/whatsbot/classes/WhatsappBot.py
@@ -103,40 +103,35 @@ class WhatsappBot:
         participantList = leftPane.find_element_by_xpath(self.xPathForConversation)
         print('Lista de conversas encontrada')
         #Infinity loop for reading new messages
-        while True:
-            print('Loop para leitura de mensagens...')
-            #Finding for the listening phone list
-            for dest in self.to:
-                try:
-                    print('Reading novas conversas para:' + dest)
-                    conversations = participantList.find_elements_by_xpath(self.xPathForListener.replace("{name}", dest))
-                    print('Conversas encontradas: ' + str(len(conversations)))
+        print('Loop para leitura de mensagens...')
+        #Finding for the listening phone list
+        for dest in self.to:
+            try:
+                print('Reading novas conversas para:' + dest)
+                conversations = participantList.find_elements_by_xpath(self.xPathForListener.replace("{name}", dest))
+                print('Conversas encontradas: ' + str(len(conversations)))
+                time.sleep(self.smallInterval)
+                for conversation in conversations:
+                    print('Conversa encontrada --> ' + conversation.text)
+                    ##TODO: Starts from new message them try to find the title or do ../../ click
+                    newConversation = conversation.find_element_by_xpath(self.xPathForNonRead.replace("{name}", dest))
+                    print('Nova conversa: ' + str(newConversation) + ' - txt: ' + newConversation.text)
+                    conversation.click()
                     time.sleep(self.smallInterval)
-                    for conversation in conversations:
-                        print('Conversa encontrada --> ' + conversation.text)
-                        ##TODO: Starts from new message them try to find the title or do ../../ click
-                        newConversation = conversation.find_element_by_xpath(self.xPathForNonRead.replace("{name}", dest))
-                        print('Nova conversa: ' + str(newConversation) + ' - txt: ' + newConversation.text)
-                        conversation.click()
-                        time.sleep(self.smallInterval)
-                        conversationDiv = self.driver.find_element_by_xpath("//div[@id='main']")
-                        print('Conversa encontrada, buscando textos')
-                        time.sleep(self.smallInterval)
-                        #TODO This should be a functional call (reading texts)
-                        texts = conversationDiv.find_elements_by_xpath(".//*[contains(@data-pre-plain-text,'27/07/2020')]")
-                        for text in texts:
-                            print( html2text.html2text(text.get_attribute("data-pre-plain-text")) + html2text.html2text(text.text) )
-                            #TODO Dialogflow and no more echo service
-                             #[0] is search in the conversation, [1] is chatbox
-                            chatboxes = self.driver.find_elements_by_class_name(self.inputClass)
-                            time.sleep(self.smallInterval)
-                            print('Chatboxs encontrados: ' + str(len(chatboxes)))
-                            chatboxes[1].click()
-                            chatboxes[1].clear()
-                            chatboxes[1].send_keys(html2text.html2text(text.text) + "!Te amo!")
-                            time.sleep(self.smallInterval)
-                        time.sleep(self.longInterval)
-                except Exception as ex:
-                    print('Não foram encontradas novas mensagens', ex)
-                time.sleep(self.longInterval)
-                
+                    conversationDiv = self.driver.find_element_by_xpath("//div[@id='main']")
+                    print('Conversa encontrada, buscando textos')
+                    time.sleep(self.smallInterval)
+                    #TODO This should be a functional call (reading texts)
+                    texts = conversationDiv.find_elements_by_xpath(".//*[contains(@data-pre-plain-text,'27/07/2020')]")
+                    for text in texts:
+                        print( html2text.html2text(text.get_attribute("data-pre-plain-text")) + html2text.html2text(text.text) )
+                        #TODO Dialogflow and no more echo service
+                            #[0] is search in the conversation, [1] is chatbox
+                        chatboxes = self.driver.find_elements_by_class_name(self.inputClass)
+                        print('Chatboxs encontrados: ' + str(len(chatboxes)))
+                        chatboxes[1].clear()
+                        chatboxes[1].send_keys(html2text.html2text(text.text))
+                        chatboxes[1].click()
+            except Exception as ex:
+                print('Não foram encontradas novas mensagens', ex)
+            time.sleep(self.longInterval)
