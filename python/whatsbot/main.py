90f8e2c6529d972e624c7ef52c5f71b25171d7f3
Vieira, Victor (Victor) <vhrodriguesv@avaya.com>
2020-08-03 14:38:37 -0300

    Added endpoint

--- python/whatsbot/main.py
+++ python/whatsbot/main.py
@@ -1,5 +1,37 @@
 from classes.WhatsappBot import WhatsappBot
 import sys
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+from threading import Thread
+
+class WhatsThread(Thread):
+    def __init__ (self, bot, message):
+        Thread.__init__(self)
+        self.message = message
+        self.bot = bot
+
+    def run(self):
+        obj = json.loads(self.message)
+        print(obj['participants'])
+        print(obj['text'])
+        self.bot.SendMessages(obj['participants'], obj['text'])
+                      
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    bot = WhatsappBot()
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'OK')
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        whats_thread = WhatsThread(bot, post_body)
+        whats_thread.start()
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('0.0.0.0', 8000)
+    httpd = server_class(server_address, handler_class)
+    print('Starting Server in the port 8000')
+    httpd.serve_forever()
 
 #TODO: Read new messages method
 #TODO: V1: How to read a file in Phyton (Java Endpoint will override the message files in the dir, phyton will read and send)
@@ -25,6 +57,10 @@ if (len(sys.argv) > 1):
     #read whatsapp web
     if command.__eq__("read"):
         bot.ReadMessages()
+    if command.__eq__("server"):
+        run(handler_class=SimpleHTTPRequestHandler)
 		
 else:
     print("This program requires an argument")
+
+
