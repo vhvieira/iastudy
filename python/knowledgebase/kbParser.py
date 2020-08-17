72f7c7592f2fbf6585b18fbc0021673d1f7753f2
Vieira, Victor (Victor) <vhrodriguesv@avaya.com>
2020-08-17 09:37:00 -0300

    Knowledge bases insert

---
+++ python/knowledgebase/kbParser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import pandas as pd
+questions = []
+answers = []
+with open('faq.txt') as ifp:
+    for line in ifp.readlines():
+        pieces = line.strip().split(':')
+        if len(pieces) >= 2:
+            term = pieces[0]
+            definition = ':'.join(pieces[1:]).strip()
+            if len(definition) > 10:
+                questions.append("What is {}".format(term))
+                answers.append(definition)
+df = pd.DataFrame.from_dict({
+    'question': questions,
+    'answer': answers
+})
+df.to_csv('knowledge_base.csv', index=False, header=False)
